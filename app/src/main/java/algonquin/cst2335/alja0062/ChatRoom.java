604aeac35a889a905dfdba73192026d8aa3f8b9f
CavalierAhmad <alja0062@algonquinlive.com>
2023-07-07 18:38:22 +0300

    Added Send button and click event

--- app/src/main/java/algonquin/cst2335/alja0062/ChatRoom.java
+++ app/src/main/java/algonquin/cst2335/alja0062/ChatRoom.java
@@ -7,40 +7,72 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 import algonquin.cst2335.alja0062.databinding.ActivityChatRoomBinding;
+import algonquin.cst2335.alja0062.databinding.SentMessageBinding;
 
 public class ChatRoom extends AppCompatActivity {
 
-    class RowHolder extends RecyclerView.ViewHolder {
-        public RowHolder(@NonNull View itemView){
-            super(itemView);
-        }
-    }
+    // Inner class section
+        class RowHolder extends RecyclerView.ViewHolder {
+            TextView message;
+            TextView time;
 
-    ActivityChatRoomBinding binding;
+            public RowHolder(@NonNull View itemView){
+                super(itemView);
+                message = itemView.findViewById(R.id.message);
+                time = itemView.findViewById(R.id.time);
+            }
+        }
+
+    // This ChatRoom attributes
+        ActivityChatRoomBinding binding;
+        ArrayList<String> messages = new ArrayList<>();
+        private RecyclerView.Adapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         // variable binding section
-        binding = ActivityChatRoomBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-        // load variables
-        binding.list.setAdapter(new RecyclerView.Adapter<RowHolder>() {
+            binding = ActivityChatRoomBinding.inflate(getLayoutInflater());
+            setContentView(binding.getRoot());
+
+        // events
+            // "Send" button
+            binding.send.setOnClickListener(click -> {
+                messages.add(binding.textField.getText().toString());
+                adapter.notifyItemInserted(messages.size()-1);
+                binding.textField.setText(""); // clear text
+            });
+
+        // LIST ADAPTER SECTION
+        binding.list.setAdapter(adapter = new RecyclerView.Adapter<RowHolder>() {
+
             @NonNull
             @Override
             public RowHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-                return null;
+                SentMessageBinding binding = SentMessageBinding.inflate(getLayoutInflater());
+                return new RowHolder(binding.getRoot());
             }
 
             @Override
             public void onBindViewHolder(@NonNull RowHolder holder, int position) {
-
+                String obj = messages.get(position);
+                holder.message.setText(obj);
+                holder.time.setText("");
             }
 
             @Override
             public int getItemCount() {
+                return messages.size();
+            }
+
+            @Override
+            public int getItemViewType(int position) {
                 return 0;
             }
         });
